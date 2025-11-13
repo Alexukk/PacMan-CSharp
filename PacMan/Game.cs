85acc355f88cc1bdbc57a4292ac443022d3eeb64
Alex <alexukrok@gmail.com>
2025-11-13 15:43:21 +0200

    Moving logic added (no wals limits yet)

--- PacMan/Game.cs
+++ PacMan/Game.cs
@@ -4,17 +4,54 @@
     {
         public static void Start(string map_path)
         {
+            Console.CursorVisible = false;
+            ConsoleKeyInfo key = new ConsoleKeyInfo('w', ConsoleKey.W, false, false, false);
             char[,] map = null;
             map = mapUtils.ReadMap(map_path);
-            mapUtils.DrawMap(map);
-             
+
+            int pacx = 1;
+            int pacy = 1;
+
             while (true)
             {
-                Console.SetCursorPosition(1, 1);
-                Console.WriteLine('@');
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                mapUtils.DrawMap(map);
 
-                Thread.Sleep(500);
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.SetCursorPosition(pacx, pacy);
+                Console.Write('@');
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                key = Console.ReadKey(true);
+                HandleInput(map, ref pacx, ref pacy, key);
+
+                Console.SetCursorPosition(34, 1);
+                Console.Write(key.KeyChar);
             }
+
+        }
+
+        private static void HandleInput(char[,] map, ref int pacx, ref int pacy, ConsoleKeyInfo key)
+        {
+
+            if (key.Key == ConsoleKey.UpArrow)
+            {
+                pacy--;
+            }
+            else if (key.Key == ConsoleKey.DownArrow)
+            {
+                pacy++;
+            }
+            else if (key.Key == ConsoleKey.LeftArrow)
+            {
+                pacx--;
+            }
+            else if (key.Key == ConsoleKey.RightArrow)
+            {
+                pacx++;
+            }
+
 
         }
     }
